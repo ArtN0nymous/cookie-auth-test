8bf42d5f8321237579c0949dc7bd3a3ae1ccd896
ArtN0nymous <jr616934@gmail.com>
2025-10-16 16:39:14 -0600

    Set up initial iOS project structure with Capacitor integration, updated app ID, and added necessary configuration files. Disabled analytics in Angular CLI configuration and included iOS-specific files such as Podfile, Info.plist, and launch screen assets.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cookie.auth.test',
   appName: 'cookie-auth-test',
   webDir: 'www'
 };
